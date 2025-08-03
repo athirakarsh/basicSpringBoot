b26891abd885854debd9b965e1f1bbf7ca86bc6b
krath <krath@Athira-PC>
2025-08-03 21:55:43 +0300

    test

---
+++ my-webapp/src/main/java/com/athira/my_webapp/MyWebappApplication.java
@@ -0,0 +1,13 @@
+package com.athira.my_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebappApplication.class, args);
+	}
+
+}
---
+++ my-webapp/src/test/java/com/athira/my_webapp/MyWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athira.my_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
